3eddf342e1a84a88d7093f9fcb1133a4ab180237
ABISHEK PANNEERSELVAM <abishek.cyber.647@gmail.com>
2026-06-15 17:39:12 +0530

    Add files via upload

---
+++ owasp_lab/broken-access/beginner/profile-access/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Component() {
+  return (
+    <>
+<div className="row justify-content-center">
+    <div className="col-md-8 text-center">
+        <h1 className="display-4 fw-bold mb-4">Welcome to Profile Access Lab</h1>
+        <p className="lead">In this challenge, you need to find the administrator's secret information.</p>
+        <hr />
+        <div className="mt-4">
+            <a href="login" className="btn btn-primary btn-lg px-5">Start Challenge</a>
+        </div>
+    </div>
+</div>
+{/* PHP code removed */}
+
+    </>
+  );
+}
