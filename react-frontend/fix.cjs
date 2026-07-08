5e948e7656bd6a81c95f6aa6dc18eb0133086f0c
ABISHEK PANNEERSELVAM <abishek.cyber.647@gmail.com>
2026-07-08 20:43:21 +0530

    Delete react-frontend directory

--- react-frontend/fix.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/pages/Home.tsx', 'utf-8');
-content = content.replace(/src="images\//g, 'src="/images/');
-fs.writeFileSync('src/pages/Home.tsx', content);
